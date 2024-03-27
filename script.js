bafbb43ce73e6907a907a1e0c381a4ff95072d97
PsammystPrime <samuelprime384@gmail.com>
2024-03-27 14:27:34 +0300

    Update carousel execution

---
+++ script.js
@@ -0,0 +1,44 @@
+function carouselPreview() {
+  const display = document.querySelector(".display");
+  const next = document.querySelector(".next");
+  next.addEventListener("click", nextImg);
+  const prev = document.querySelector(".prev");
+  prev.addEventListener("click", prevImg);
+
+  //Initialize your images
+  const one = document.createElement("img");
+  one.src = "./Gamer_girl.png";
+  const two = document.createElement("img");
+  two.src = "./alien.jpg";
+  const three = document.createElement("img");
+  three.src = "./exotic.png";
+  const four = document.createElement("img");
+  four.src = "Beautiful.jpg";
+  const five = document.createElement("img");
+  five.src = "bridge.png";
+  const six = document.querySelector(".homeImg");
+  //Push them to an array
+  const imageArray = [one, two, three, four, five, six];
+
+  index = 0;
+
+  function nextImg() {
+    if (index === 5) {
+      index = 0;
+    } else {
+      index++;
+    }
+    display.innerHTML = "";
+    display.appendChild(imageArray[index]);
+  }
+  function prevImg() {
+    if (index === 0) {
+      index = imageArray.length - 1;
+    } else {
+      index--;
+    }
+    display.innerHTML = "";
+    display.appendChild(imageArray[index]);
+  }
+}
+carouselPreview();
